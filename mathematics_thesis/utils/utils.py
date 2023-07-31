dce5ff5d61d3315d75026db6f9a73b5f785b5d76
juan190199 <juanmanueldasilvatome@gmail.com>
2023-07-31 15:45:09 +0200

    ~// Renaming method (calculate_accuracy -> accuracy_score) accuracy_scores implements error handling choose_random_idxs method enables more flexibility

--- mathematics_thesis/utils/utils.py
+++ mathematics_thesis/utils/utils.py
@@ -19,15 +19,62 @@ def interpolate_points(x1, x2, alpha):
     return alpha * x1 + (1 - alpha) * x2
 
 
-def choose_random_idxs(key, dataset, num_points=2):
-    num_samples = dataset.shape[0]
-    idxs = random.choice(key, num_samples, shape=(num_points, ), replace=False)
-    return idxs
+# def choose_random_idxs(key, labels, n_idxs=2):
+#     unique_labels = np.unique(labels)
+#     label1, label2 = random.choice(key, unique_labels, shape=(2,), replace=False)
+#     idxs_label1 = np.where(labels == label1)[0]
+#     idxs_label2 = np.where(labels == label2)[0]
+#
+#     key, subkey1, subkey2 = random.split(key, 3)
+#     idx1 = random.choice(subkey1, idxs_label1)
+#     idx2 = random.choice(subkey2, idxs_label2)
+#
+#     return idx1, idx2
 
 
-def calculate_accuracy(predicted, ground_truth):
-    correct = np.sum(predicted == ground_truth)
-    total = len(ground_truth)
+def choose_random_idxs(key, labels, n_idxs=2):
+    unique_labels = np.unique(labels)
+    n_labels = unique_labels.shape[0]
+
+    if n_labels < n_idxs:
+        raise ValueError('Number of indices to select is greater than number of unique labels.')
+
+    # ToDo: Raise error when there are not enough labels to pick at least n_idx data point with different labels
+
+    idxs = np.array([], dtype=np.int32)
+    for _ in range(n_idxs):
+        key, subkey = random.split(key)
+        label = random.choice(subkey, unique_labels)
+        label_idxs = np.where(labels == label)[0]
+
+        key, subkey = random.split(key)
+        idx = random.choice(subkey, label_idxs)
+        idxs = np.append(idxs, idx)
+        labels = np.delete(labels, idx)
+
+    return idxs.astype(int)
+
+
+    label1, label2 = random.choice(key, unique_labels, shape=(2,), replace=False)
+    idxs_label1 = np.where(labels == label1)[0]
+    idxs_label2 = np.where(labels == label2)[0]
+
+    key, subkey1, subkey2 = random.split(key, 3)
+    idx1 = random.choice(subkey1, idxs_label1)
+    idx2 = random.choice(subkey2, idxs_label2)
+
+    return idx1, idx2
+
+
+def accuracy_score(y_true, y_pred):
+    if y_true.shape != y_pred.shape:
+        raise ValueError("Shape mismatch between y_true and y_pred")
+
+    # Flatten the arrays if they have shape (len(data), 1)
+    if len(y_true.shape) == 2:
+        y_true = np.squeeze(y_true)
+        y_pred = np.squeeze(y_pred)
+
     # Calculate accuracy
-    accuracy = correct / total
+    accuracy = np.mean(y_true == y_pred)
     return accuracy
