cd01d5eed0f22cff21a2c5186b8b9eb1db150f73
juan190199 <juanmanueldasilvatome@gmail.com>
2023-07-31 15:46:34 +0200

    ~// General changes: fontsize, markersize, linewidth, colors. Solving bug when plotting interpolation

--- mathematics_thesis/utils/viz.py
+++ mathematics_thesis/utils/viz.py
@@ -7,9 +7,9 @@ def format_plot(x=None, y=None):
     # plt.grid(False)
     ax = plt.gca()
     if x is not None:
-        plt.xlabel(x, fontsize=20)
+        plt.xlabel(x)
     if y is not None:
-        plt.ylabel(y, fontsize=20)
+        plt.ylabel(y)
 
 
 def finalize_plot(shape=(1, 1)):
@@ -19,35 +19,34 @@ def finalize_plot(shape=(1, 1)):
     plt.tight_layout()
 
 
-def plot_fn(train, test, *fs):
+def plot_fn(train, test, xlabel=None, ylabel=None, *fs):
     train_xs, train_ys = train
-
-    plt.plot(train_xs, train_ys, 'ro', markersize=4, label='train')
+    plt.plot(train_xs, train_ys, 'mo', markersize=3, label='Train')
 
     if test != None:
         test_xs, test_ys = test
-        plt.plot(test_xs, test_ys, 'k--', linewidth=3, label='$f(x)$')
+        plt.plot(test_xs, test_ys, 'k--', linewidth=1.5, label='$f(x)$')
 
         for f in fs:
-            plt.plot(test_xs, f(test_xs), '-', linewidth=3)
+            plt.plot(test_xs, f(test_xs), '-', linewidth=1.5)
 
     plt.xlim([-np.pi, np.pi])
     plt.ylim([-1.5, 1.5])
 
-    format_plot('$x$', '$f$')
+    format_plot(xlabel, ylabel)
 
 
-def plot_interpolation(train, x_test, *fs):
+def plot_interpolation(train, x_test, xlabel=None, ylabel=None, *fs):
     x_train, y_train = train
-    train_alphas = np.array((0, 1))
-    plt.plot(train_alphas, y_train, 'ro', markersize=5, label='train')
+    train_alphas = np.array((1, 0))
+    plt.plot(train_alphas, y_train, 'mo', markersize=3, label='train')
 
     if x_test != None:
         # plt.plot(x_test, test_ys, 'k--', linewidth=3, label='$f(x)$')
         for f in fs:
-            plt.plot(x_test, f(x_test), '-', linewidth=3)
+            plt.plot(x_test, f(x_test), '-', linewidth=1.5)
 
     plt.xlim([-np.pi, np.pi])
     plt.ylim([-1.5, 1.5])
 
-    format_plot(r'$\alpha$', r'$f$')
+    format_plot(xlabel, ylabel)
